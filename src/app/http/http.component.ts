d13532cc4d82cdc7a143c695d889c29aa61bbbe8
dhruvburada <buradadhruv35@gmail.com>
2025-08-29 11:42:57 +0530

    fetching data on ui deleting posts and reporting errors on ui

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { HttpService } from './http.service';
-
+import { Data } from '@angular/router';
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
@@ -13,15 +13,27 @@ export class HttpComponent {
   constructor(private httpService: HttpService) { }
   userForm!: FormGroup;
   error!: string;
+  userData!: Data[];
   ngOnInit() {
     this.userForm = new FormGroup({
       'name': new FormControl(''),
       'email': new FormControl('')
     })
+    this.fetchData();
 
+  }
+  submitForm() {
+    this.httpService.sendData(this.userForm.value).subscribe(() => {
+      this.userData.push(this.userForm.value)
+      this.userForm.reset()
+    }, (err) => this.error = err.message)
+
+  }
+
+  fetchData() {
     this.httpService.getData().subscribe({
       next: data => {
-        console.log(data)
+        this.userData = data
       },
       error: (error) => {
         this.error = error.message
@@ -29,11 +41,15 @@ export class HttpComponent {
     }
     )
   }
-  submitForm() {
-    console.log(this.userForm.value)
-    this.httpService.sendData(this.userForm.value).subscribe((resData) => {
-      console.log(resData)
-    })
+
+  clearData() {
+    this.httpService.clearData().subscribe(() => {
+      this.userData = []
+    }, (err) => this.error = err.message)
+  }
+
+  deleteUser(id: string) {
+    this.httpService.deleteUser(id).subscribe(() => { this.userData = this.userData.filter((data: Data) => data["id"] !== id) }, (err) => this.error = err.message)
   }
 
 }
--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -10,25 +10,27 @@ import { map } from 'rxjs/operators'; // ✅ Correct
 export class HttpService {
 
   constructor(private httpClient: HttpClient) { }
-
+  baseUrl: string = 'https://angular-test-b92d0-default-rtdb.firebaseio.com/posts'
   // getData(): OperatorFunction<any> {
   //   return this.httpClient.get('https://angular-test-b92d0-default-rtdb.firebaseio.com/posts.json').pipe(map(data) => { ...data })
   // }
 
   sendData(data: any): Observable<any> {
-    return this.httpClient.post('https://angular-test-b92d0-default-rtdb.firebaseio.com/posts.json', data)
+    return this.httpClient.post(this.baseUrl + ".json", data)
+
   }
 
 
   getData(): Observable<any[]> {
     return this.httpClient.get<{ [key: string]: { name: string, email: string } }>(
-      'https://angular-test-b92d0-default-rtdb.firebaseio.com/'
+      this.baseUrl + ".json"
     ).pipe(
       map(data => {
-        const postsArray: { id: string, name: string, email: string }[] = [];
+        let postsArray: { id: string, name: string, email: string }[] = [];
         if (data) {
           for (const key in data) {
             if (data.hasOwnProperty(key)) {
+
               postsArray.push({ id: key, ...data[key] });
             }
           }
@@ -36,6 +38,14 @@ export class HttpService {
         return postsArray;
       })
     );
+  }
+
+  clearData() {
+    return this.httpClient.delete(this.baseUrl + ".json")
+  }
+
+  deleteUser(id: string) {
+    return this.httpClient.delete(this.baseUrl + '/' + id + '.json')
   }
 
 }
