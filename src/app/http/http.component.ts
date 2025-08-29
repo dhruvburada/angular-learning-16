59ebc70c73f2416ff2cefa416c411891a7246f1e
dhruvburada <buradadhruv35@gmail.com>
2025-08-29 12:04:03 +0530

    used subject to handle errors

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,32 +1,43 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { HttpService } from './http.service';
 import { Data } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
   styleUrls: ['./http.component.css']
 })
-export class HttpComponent {
+export class HttpComponent implements OnInit, OnDestroy {
 
   constructor(private httpService: HttpService) { }
   userForm!: FormGroup;
-  error!: string;
+  error = new Subject<string>();
+  errorMessage: null | string = null;
   userData!: Data[];
+  errorSubscription!: Subscription;
   ngOnInit() {
     this.userForm = new FormGroup({
       'name': new FormControl(''),
       'email': new FormControl('')
     })
-    this.fetchData();
 
+    this.fetchData();
+    this.errorSubscription = this.error.subscribe((err) => {
+      this.errorMessage = err
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    this.errorSubscription.unsubscribe()
   }
   submitForm() {
     this.httpService.sendData(this.userForm.value).subscribe(() => {
       this.userData.push(this.userForm.value)
       this.userForm.reset()
-    }, (err) => this.error = err.message)
+    }, (err) => this.error.next(err.message))
 
   }
 
@@ -35,8 +46,8 @@ export class HttpComponent {
       next: data => {
         this.userData = data
       },
-      error: (error) => {
-        this.error = error.message
+      error: (err) => {
+        this.error.next(err.message)
       }
     }
     )
@@ -45,11 +56,11 @@ export class HttpComponent {
   clearData() {
     this.httpService.clearData().subscribe(() => {
       this.userData = []
-    }, (err) => this.error = err.message)
+    }, (err) => this.error.next(err.message))
   }
 
   deleteUser(id: string) {
-    this.httpService.deleteUser(id).subscribe(() => { this.userData = this.userData.filter((data: Data) => data["id"] !== id) }, (err) => this.error = err.message)
+    this.httpService.deleteUser(id).subscribe(() => { this.userData = this.userData.filter((data: Data) => data["id"] !== id) }, (err) => this.error.next(err.message))
   }
 
 }
